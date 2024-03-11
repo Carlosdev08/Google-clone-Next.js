53fed14689b36e1a2b63f64587b9e0ea69f8fa44
Carlos Del Jesus <144354364+Carlosdev08@users.noreply.github.com>
2024-03-11 16:30:41 +0100

    add body section

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,21 @@
 import HomeHeader from "googleclon/components/HomeHeader";
+import HomeSearch from "googleclon/components/HomeSearch";
+import Image from "next/image";
 export default function Home() {
   return (
     <>
       <HomeHeader />
+      <div className="flex flex-col items-center mt-24">
+        <Image
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1200px-Google_2015_logo.svg.png"
+          alt="Google logo"
+          width={300}
+          height={100}
+          priority
+          style={{ with: "auto" }}
+        />
+        <HomeSearch />
+      </div>
     </>
   );
 }
